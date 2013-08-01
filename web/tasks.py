4817c36e5a74368e9dfc5054930bde66af650763
Honza Pokorny <me@honza.ca>
2013-07-31 21:56:34 -0300

    Rework build process

--- web/tasks.py
+++ web/tasks.py
@@ -1,34 +1,66 @@
+import os
+import logging
+
 from celery import task
 from docker.client import Client
-import tempfile
 from git import Repo
-import shutil
+
+from django.conf import settings
+
+
+logger = logging.getLogger(__name__)
+REPOSITORY_DIR = getattr(settings, 'REPOSITORY_DIR', None)
 
 
 @task
 def run_build(build):
-    prj = build.project
+    project = build.project
+
     # docker client
     c = Client(build.host)
-    tmp_dir = tempfile.mkdtemp()
-    # clone repo
-    repo = Repo.clone_from(prj.repository, tmp_dir)
+
+    project_dir = os.path.join(REPOSITORY_DIR, project.slug)
+    main_repo_path = os.path.join(project_dir, 'clone')
+
+    if not os.path.exists(main_repo_path):
+
+        # assert project repo present
+        os.path.makedirs(project_dir)
+
+        # clone repo
+        repo = Repo.clone_from(project.repository, main_repo_path)
+
+    else:
+        repo = Repo(main_repo_path)
+        remote = repo.remote()
+        remote.pull()
+
+    # build_path is a local clone of the project and it's named after the
+    # current build's commit sha
+    build_path = os.path.join(project_dir, build.commit_sha)
+    repo = Repo.clone_from(main_repo_path, build_path)
     g = repo.git
-    # checkout commit from push
+
     g.checkout(build.commit_sha)
-    repo_name = '/'.join(['wharfci', prj.name])
-    image_name = ':'.join([repo_name, build.commit_sha])
-    img_id, res = c.build(tmp_dir)
+
+    image_name = ':'.join([project.slug, build.commit_sha])
+
+    img_id, res = c.build(build_path)
+
     # create image
-    c.tag(img_id, repository=repo_name, tag=build.commit_sha)
-    # cleanup
-    shutil.rmtree(tmp_dir)
+    c.tag(img_id, repository=project.slug, tag=build.commit_sha)
+
     # run build command from new image and report output
-    container = c.create_container(image_name, prj.build_command,
-        stdin_open=True, tty=True)
+    container = c.create_container(image_name, project.build_command,
+                                   stdin_open=True, tty=True)
+
     container_id = container.get('Id')
+
     c.start(container_id)
     c.wait(container_id)
+
     out = c.logs(container_id)
     build.result = out
     build.save()
+
+    # TODO: Update build status based on exit code
