6d16e7e53f4bf89aba32f160431cd20f5a28accd
Evan Hazlett <ejhazlett@gmail.com>
2013-08-01 03:13:19 -0400

    minor ui updates

--- wharfci/views.py
+++ wharfci/views.py
@@ -1,7 +1,11 @@
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
+from django.core.urlresolvers import reverse
 
 def index(request):
     ctx = {}
+    # show dashboard if logged in
+    if request.user.is_authenticated():
+        return redirect(reverse('dashboard'))
     return render_to_response('index.html', ctx,
         context_instance=RequestContext(request))
